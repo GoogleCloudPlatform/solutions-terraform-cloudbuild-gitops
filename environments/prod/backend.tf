465d30bf6aa365c5b286518f00c1869bd05aa50b
atanu-ghosh <atanu.ghosh@gmail.com>
2019-09-30 10:47:44 +0530

    Test PR Merge

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-terraform {
+# Noqw creatinbg anotrhre PR   to see the tf apply check
+terraform {  
   backend "gcs" {
     bucket = "solid-choir-254209-tfstate"
     prefix = "env/prod"
