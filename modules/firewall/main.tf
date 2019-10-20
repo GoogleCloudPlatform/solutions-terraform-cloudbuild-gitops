00d40eda0f4ef2f8310dbfc653a65f279c53329d
Sudarshana Melukote <smelukote@gmail.com>
2019-10-19 17:04:04 -0700

    Update main.tf
    Fixing http firewall target

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#fix the "http-server**2**" typo in the target_tags field.
+#The value must be "http-server".
 
 locals {
   network = "${element(split("-", var.subnet), 0)}"
