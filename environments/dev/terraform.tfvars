2b2c1d253666ca9803f053e13fe42c9f45c5064f
theo <tgelpi@gmail.com>
2019-11-17 18:37:32 -0500

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="tbear-project-v9"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "tbear-project-v9-tfstate"
     prefix = "env/prod"
   }
 }
