97f74e980599c058ac64dc86651d9fba9fc56c87
ptiringo <ptiringo@gmail.com>
2019-11-28 14:32:21 +0900

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="learning-gcp-260106"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "learning-gcp-260106-tfstate"
     prefix = "env/prod"
   }
 }
