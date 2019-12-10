1ed1bc4e81f2d06390ff5da445fdfc9275fcf3e4
Vinayak <vinayakpandeyit@gmail.com>
2019-12-10 12:48:29 +0530

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="playground-s-11-29fa00"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "playground-s-11-29fa00-tfstate"
     prefix = "env/prod"
   }
 }
