97316ebdedb6d96c41e567b5716116b97fe8d19d
arc651 <arjunrchandra@gmail.com>
2020-02-16 12:40:59 -0800

    delete modules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,20 +21,9 @@ provider "google" {
   project = "${var.project}"
 }
 
-#module "vpc" {
-#  source  = "../../modules/vpc"
-#  project = "${var.project}"
-#  env     = "${local.env}"
-#}
 
 module "http_server" {
   source  = "../../modules/http_server"
   project = "${var.project}"
   subnet  = "sub-prv-usw1-01"
 }
-
-#module "firewall" {
-#  source  = "../../modules/firewall"
-#  project = "${var.project}"
-#  subnet  = "${module.vpc.subnet}"
-#}
