ddd342e78371f71b9bf3201ab0faee2a84562867
Michael Intindola <admin@mintindola.workshop.ongcp.co>
2020-05-15 11:11:54 -0400

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "css-testing-pipeline-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="css-testing-pipeline"
