4834df2dc82a54ed8ed5729459b7e497d4359d16
dbvan <bavanduongltc@gmail.com>
2020-07-26 11:23:18 +0700

    Update project id

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "premium-silicon-282607-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="premium-silicon-282607"
