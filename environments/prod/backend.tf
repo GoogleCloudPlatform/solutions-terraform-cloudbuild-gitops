70984eb3334ccd7a52bd9e5daf3bd9dbb66245ef
Aaron M Little <aaron.m.little@gmail.com>
2020-08-12 11:35:58 -0700

    Update project ID's and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "acn-vegaai-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="acn-vegaai"
