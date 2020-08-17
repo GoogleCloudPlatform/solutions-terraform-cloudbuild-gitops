3d6f373ef158f5f8a289738f8e9e2bf95947b430
tiaa-chenru <rugui.chen@tiaa.org>
2020-08-17 13:42:10 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "orbital-gantry-285002-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="orbital-gantry-285002"
