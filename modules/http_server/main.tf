5d8789e28ea3b0ece98d0baf3491f158c1b42246
SunilPillai100 <59488898+SunilPillai100@users.noreply.github.com>
2020-08-18 16:45:06 -0600

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -34,7 +34,7 @@ resource "google_compute_instance" "http_server" {
   resource "google_compute_instance" "http_server" {
   project      = "${var.project}"
   zone         = "us-west1-a"
-  name         = "${local.network}-apache3-instance"
+  name         = "${local.network}-apache4-instance"
   machine_type = "f1-micro"
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
