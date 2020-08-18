d5eb0f70668d907428953718bcb5b7ba4c66f84a
Sunil Pillai Teck <sunil.pillai@teck.com>
2020-08-18 16:39:45 -0600

    New VM

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -31,6 +31,20 @@ resource "google_compute_instance" "http_server" {
     }
   }
 
+  resource "google_compute_instance" "http_server" {
+  project      = "${var.project}"
+  zone         = "us-west1-a"
+  name         = "${local.network}-apache3-instance"
+  machine_type = "f1-micro"
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
   network_interface {
     subnetwork = "${var.subnet}"
 
