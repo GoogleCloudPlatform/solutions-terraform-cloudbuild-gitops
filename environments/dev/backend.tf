f5ae4abb0ddd8fd944e4153b102c9ca6ebb86ce5
dtwistednerd <onzeadibe@gmail.com>
2020-09-05 17:13:30 +0000

    update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "valiant-store-288614-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="valiant-store-288614"
