00ea3fd6d6775517fe12abe7935c720ec5f7fdcc
jklang <klang.joakim@gmail.com>
2020-09-09 08:23:29 +0000

    remove output

--- modules/http_server/outputs.tf
+++ modules/http_server/outputs.tf
@@ -17,6 +17,3 @@ output "instance_name" {
   value = "${google_compute_instance.http_server.name}"
 }
 
-output "external_ip" {
-  value = "${google_compute_instance.http_server.network_interface.0.access_config.0.nat_ip}"
-}
