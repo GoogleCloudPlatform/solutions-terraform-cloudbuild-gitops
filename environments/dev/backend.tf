5e409e79abf485abc729b8f518c792a91f3e35e2
jklang <klang.joakim@gmail.com>
2020-09-09 07:01:33 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "king-ie-joakim-klang-sandbox-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="king-ie-joakim-klang-sandbox"
