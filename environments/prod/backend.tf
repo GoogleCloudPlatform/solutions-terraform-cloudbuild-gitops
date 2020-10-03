c5384cc7f6b6d1406857679be2f262dc369f14ce
Arnaud Bakoula <arnaud.bakoula@gmail.com>
2020-10-03 22:02:03 +0200

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "magnetic-hawk-276008-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="magnetic-hawk-276008"
