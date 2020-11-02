7eeb63f7b1f49f441e9b4f887b491bac7b4dedd4
Yasir Saddiq <yasir.saddiq@ingka.com>
2020-11-02 17:23:41 +0100

    Updates for later version

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,23 +18,23 @@ locals {
 }
 
 provider "google" {
-  project = "${var.project}"
+  project = var.project
 }
 
 module "vpc" {
   source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
+  project = var.project
+  env     = local.env
 }
 
 module "http_server" {
   source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+  project = var.project
+  subnet  = module.vpc.subnet
 }
 
 module "firewall" {
   source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+  project = var.project
+  subnet  = module.vpc.subnet
 }
