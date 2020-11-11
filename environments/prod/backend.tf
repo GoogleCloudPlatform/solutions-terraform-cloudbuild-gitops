9fcf572335c90b65d75b7c51fdcb1e1c0d70d0f6
manuel-balandra <manuel.balandra@xenix.tech>
2020-11-11 16:22:42 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "true-sprite-294918-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="true-sprite-294918"
