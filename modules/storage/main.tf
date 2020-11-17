bc54c91c23f5816c624f51d5388844279f1eb96e
CapMultiCloud <cap.multicloud@gmail.com>
2020-11-17 17:27:26 +0000

    Storage bucket and files

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -28,8 +28,12 @@ resource "google_storage_bucket" "cap-archive-mds" {
   }
 }
 
+#data "local_file" "startup_script" {
+#    filename = "${path.module}/files/cap-template-ss.sh"
+#}
+
 resource "google_storage_bucket_object" "mig-sftp-ss" {
   name   = "cap-template-ss.sh"
-  source = "${var.source}"
+  source = file("${path.module}/files/cap-template-ss.sh")
   bucket = "${google_storage_bucket.cap-archive-mds.name}"
 }
