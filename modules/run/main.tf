89e3e562fe635e3ec3824c971f4f822837778234
CapMulticloud <cap.multicloud@gmail.com>
2020-12-07 11:13:00 +0000

    test run

--- modules/run/main.tf
+++ modules/run/main.tf
@@ -61,3 +61,21 @@ resource "google_cloud_run_service" "credit-approval-app" {
         }
 
 }
+
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.credit-approval-app.location
+  project     = google_cloud_run_service.credit-approval-app.project
+  service     = google_cloud_run_service.credit-approval-app.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
