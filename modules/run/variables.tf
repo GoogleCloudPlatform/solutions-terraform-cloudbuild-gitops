51cbb60d5224fb500976d86160317af89e2f0bd3
CapMulticloud <cap.multicloud@gmail.com>
2020-12-07 10:21:58 +0000

    updated variables for run

--- modules/run/variables.tf
+++ modules/run/variables.tf
@@ -1,8 +1,3 @@
 variable "project" {}
 variable "env" {}
 variable "region" {}
-variable "mds" {}
-variable "main_bucket" {}
-variable "sa_email" {}
-variable "cf_sendmail_pubsub" {}
-variable "cf_triggerworkflow_pubsub" {}
