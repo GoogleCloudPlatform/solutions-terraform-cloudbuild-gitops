c67ebc06daa116f304b8acc5b095e1af8d3bd9ab
Mihai Vasilache <mihai@antispo.com>
2020-12-21 07:28:06 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "aip-rentz-cloud-build-github-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="aip-rentz-cloud-build-github"
