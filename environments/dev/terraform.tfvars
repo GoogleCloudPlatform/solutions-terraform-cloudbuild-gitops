0e625c047bf13db846362afd7530d3e32db71dab
Alex Bulankou <alexbu@google.com>
2021-01-28 14:28:59 -0800

    Updating project IDs and bucket info

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="alexbu-20200128-gitops"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "alexbu-20200128-gitops-tfstate"
     prefix = "env/prod"
   }
 }
