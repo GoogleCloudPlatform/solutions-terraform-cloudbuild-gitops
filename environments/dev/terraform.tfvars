c5df4ca3a9d20940111fec5f7a508c8d0b120038
Stefan Andrei <andreistefanciprian@gmail.com>
2021-02-08 14:50:24 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="firm-alchemy-304214"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "firm-alchemy-304214-tfstate"
     prefix = "env/prod"
   }
 }
