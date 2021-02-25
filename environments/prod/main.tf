5c9612f22fbe8da61b32ea7732defe1376e08e68
kyeswanth <yeswanthk@biarca.com>
2021-02-25 17:45:31 +0530

    updated scripts

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -26,15 +26,3 @@ module "vpc" {
   project = "${var.project}"
   env     = "${local.env}"
 }
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
