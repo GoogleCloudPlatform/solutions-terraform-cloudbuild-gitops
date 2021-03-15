d91e99c84983517566de79fcd7ecabddf242cd06
kyeswanth <yeswanthk@biarca.com>
2021-03-15 16:53:17 +0530

    Testing

--- dev/terraform/variables.tf
+++ dev/terraform/variables.tf
@@ -11,5 +11,5 @@ variable "project_id" {
 # vpc network name
 variable "vpc_network_name" {
   type = string
-  default = "infrastructure-dev-network-dev-mar15"
+  default = "infrastructure-dev-network-dev-mar-15"
 }
