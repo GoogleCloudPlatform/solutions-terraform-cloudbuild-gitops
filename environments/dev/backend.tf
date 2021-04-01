dac85dd801f628e86798eeca0f3cc45c5eae5ea0
Imane0 <najah.bentabet@gmail.com>
2021-04-01 12:26:35 +0000

    set project id

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "terraform-tutorial-309410-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="terraform-tutorial-309410"
