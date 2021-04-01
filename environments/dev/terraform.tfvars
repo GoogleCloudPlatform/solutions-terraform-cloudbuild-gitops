f814b66868085d86bd4ba775b2d94d5f4abc3505
Casey Tin <ctin@umich.edu>
2021-04-01 14:05:02 +0000

    update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="caseytin-sandbox-pso"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "caseytin-sandbox-pso-tfstate"
     prefix = "env/prod"
   }
 }
