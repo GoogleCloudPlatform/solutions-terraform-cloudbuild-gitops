a76e4f41a94f5ac16cb1d6584fa64c9c00b18bb4
Shani Levy <shaniiiiii@gmail.com>
2021-04-08 20:14:11 +1000

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -19,15 +19,3 @@ variable "project_id" {
   description = "Google Project ID."
   type        = string
 }
-
-variable "bucket_name" {
-  description = "GCS Bucket name. Value should be unique ."
-  type        = string
-  default = "my-dataops-bucket-123"
-}
-
-variable "region" {
-  description = "Google Cloud region"
-  type        = string
-  default     = "europe-west2"
-}
