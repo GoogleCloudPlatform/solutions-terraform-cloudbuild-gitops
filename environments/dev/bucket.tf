84f478e23d82f0e07f26d6ce53081ba700a912b4
Shani Levy <shaniiiiii@gmail.com>
2021-04-08 20:04:14 +1000

    Update bucket.tf

--- environments/dev/bucket.tf
+++ environments/dev/bucket.tf
@@ -1,9 +1,3 @@
-# Specify the GCP Provider
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 # Create a GCS Bucket
 resource "google_storage_bucket" "dataops_bucket" {
   name                        = var.bucket_name
