c061057bdbecb2116ece174a26ba8d584ad5738e
Shani Levy <shaniiiiii@gmail.com>
2021-04-08 20:08:20 +1000

    Update bucket.tf

--- environments/dev/bucket.tf
+++ environments/dev/bucket.tf
@@ -1,9 +1,6 @@
 # Create a GCS Bucket
 resource "google_storage_bucket" "dataops_bucket" {
-  name                        = var.bucket_name
-  project                     = var.project_id
-  location                    = var.region
-  storage_class               = "REGIONAL"
-  force_destroy               = true
-  uniform_bucket_level_access = true
+  name          = var.bucket_name
+  location      = var.region
+  force_destroy = false
 }
