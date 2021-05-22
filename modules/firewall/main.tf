fa4eb89c9f13881b071cd6676cff7be97b78960f
rajesh-lab-github <69465642+rajesh-lab-github@users.noreply.github.com>
2021-05-22 11:37:06 +0530

    Update main.tf
    Fixing http firewall target

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
+Fixing http firewall target
+"http-server**2**"
 locals {
   network = "${element(split("-", var.subnet), 0)}"
 }
