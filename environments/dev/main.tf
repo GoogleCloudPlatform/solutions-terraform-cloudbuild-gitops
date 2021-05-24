1a7d3c6f73ba4dc6976c41a3a0b071550880f39d
kumaraswami <kumaraswami85@gmail.com>
2021-05-23 22:25:31 -0500

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -52,10 +52,10 @@ provider "google" {
   zone    = "us-central1-c"
 }
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network-01"
 }
 resource "google_compute_subnetwork" "public-subnetwork" {
-  name          = "terraform-subnetwork"
+  name          = "terraform-subnetwork-01"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.name
