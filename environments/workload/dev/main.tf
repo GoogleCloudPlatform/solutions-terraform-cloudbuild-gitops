3213a77484195ea5be04ac01ae85d177c7ba237a
kumaraswami <kumaraswami85@gmail.com>
2021-05-27 11:39:56 -0500

    Update main.tf

--- environments/workload/dev/main.tf
+++ environments/workload/dev/main.tf
@@ -82,11 +82,11 @@ locals {
 #}
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network-02"
+  name = "terraform-network-03"
   auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "public-subnetwork" {
-  name          = "terraform-subnetwork-02"
+  name          = "terraform-subnetwork-03"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
