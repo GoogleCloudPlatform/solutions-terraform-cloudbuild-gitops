727e7c3d6ec69be18b66ad25bb1c5d24b387978c
kumaraswami <kumaraswami85@gmail.com>
2021-05-27 15:34:11 -0500

    Update main.tf

--- terraform/environments/workload/dev/main.tf
+++ terraform/environments/workload/dev/main.tf
@@ -91,4 +91,4 @@ resource "google_compute_subnetwork" "public-subnetwork" {
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
-} 
+}
