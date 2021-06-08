94aff795af10129c8f0dcd291422aa505cf70aed
jpl2020 <ubuntu@ar-it18715.globant.com>
2021-06-08 13:56:07 -0300

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "dceng-testing-ncr-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="dceng-testing-ncr"
