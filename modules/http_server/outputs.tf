f14b3f50e55a668665fb1c20d315082f310accd6
ChiragBarsaiya <45994332+ChiragBarsaiya@users.noreply.github.com>
2021-06-26 04:43:31 +0530

    Update outputs.tf

--- modules/http_server/outputs.tf
+++ modules/http_server/outputs.tf
@@ -14,9 +14,9 @@
 
 
 output "instance_name" {
-  value = "${google_compute_instance.web_server.name}"
+  value = "${google_compute_instance.web-server.name}"
 }
 
 output "external_ip" {
-  value = "${google_compute_instance.web_server.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.web-server.network_interface.0.access_config.0.nat_ip}"
 }
