00f401a630bb7745dad8efd025bf38870995cf79
ChiragBarsaiya <45994332+ChiragBarsaiya@users.noreply.github.com>
2021-06-26 04:27:10 +0530

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -17,7 +17,7 @@ locals {
   network = "${element(split("-", var.subnet), 0)}"
 }
 
-resource "google_compute_instance" "http_server" {
+resource "google_compute_instance" "web_server" {
   project      = "${var.project}"
   zone         = "us-west1-a"
   name         = "${local.network}-apache2-instance"
