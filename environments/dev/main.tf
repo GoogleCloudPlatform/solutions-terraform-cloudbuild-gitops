464c55942e5627cc9b15e09bdfe7672446b7da56
rahuld1288 <87582766+rahuld1288@users.noreply.github.com>
2021-07-22 15:48:17 +0530

    alerting update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,20 +21,27 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+resource "google_monitoring_alert_policy" "alert_policy0" {
+  display_name = "1 - Availability - Google Cloud HTTP/S Load Balancing Rule - Request count (filtered) [COUNT]"
+  combiner = "OR"
+  conditions {
+    display_name = "Google Cloud HTTP/S Load Balancing Rule - Request count (filtered) [COUNT]"
+    condition_threshold {
+      filter = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.label.response_code!=\"200\"" 
+      duration = "60s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 1
+      trigger {
+          count = 1
+      }
+      aggregations {
+        alignment_period = "60s"
+        per_series_aligner = "ALIGN_RATE"
+        cross_series_reducer = "REDUCE_COUNT"
+      }
+    }
+  }
+  documentation {
+    content = "The load balancer rule $${condition.display_name} has generated this alert for the $${metric.display_name}."
+  }
 }
