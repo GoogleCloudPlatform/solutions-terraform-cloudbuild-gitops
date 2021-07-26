4d1188e988abca23ab930808f569cec0dfd468a0
Nicolas Angel Surdi <nico@aeeros.la>
2021-07-26 13:44:32 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="aeros-sandbox-surdi"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "aeros-sandbox-surdi-tfstate"
     prefix = "env/prod"
   }
 }
