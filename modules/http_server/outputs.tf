ba2126243d2e4a765813a253509e910d145511a5
chamrong-pheng <72828200+chamrong-pheng@users.noreply.github.com>
2021-07-30 15:00:19 -0400

    Update outputs.tf

--- modules/http_server/outputs.tf
+++ modules/http_server/outputs.tf
@@ -17,6 +17,6 @@ output "instance_name" {
   value = "${google_compute_instance.http_server.name}"
 }
 
-#output "external_ip" {
- # value = "${google_compute_instance.http_server.network_interface.0.access_config.0.nat_ip}"
-#}
+output "external_ip" {
+  value = "${google_compute_instance.http_server.network_interface.0.access_config.0.nat_ip}"
+}
