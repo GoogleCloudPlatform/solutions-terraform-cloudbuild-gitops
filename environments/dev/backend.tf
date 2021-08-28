0127d259ea27e93c1136e5e6684fabeec9aa4378
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-08-28 15:13:20 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "demoproject-324314-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="demoproject-324314"
