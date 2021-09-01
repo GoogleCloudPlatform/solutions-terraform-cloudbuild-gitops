32713a605c93149dd9df337841688cace5bd8c38
Flavio <flavio.moura@gmail.com>
2021-08-31 23:21:41 -0300

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="$PROJECT_ID"
+project="$terraform-20210831"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "$PROJECT_ID-tfstate"
+    bucket = "$terraform-20210831-tfstate"
     prefix = "env/prod"
   }
 }
