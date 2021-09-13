36c808c98c1b0fea96b9bb9b3e75864d0177fb94
Michael Woo <mwoo@prosper.com>
2021-09-13 19:07:05 +0000

    update project IDS and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="nonprod-prosper-logging"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "nonprod-prosper-logging-tfstate"
     prefix = "env/prod"
   }
 }
