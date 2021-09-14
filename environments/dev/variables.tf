e15cb450e0cc9a3292a1e958485fff7395b0d489
monika16p <49428322+monika16p@users.noreply.github.com>
2021-09-14 17:47:35 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -14,3 +14,85 @@
 
 
 variable "project" {}
+variable "kubernetes_engine-create" {
+	type = bool
+	default = false
+}
+
+variable "kubernetes_engine-count" {
+	type = number
+	default = 1
+}
+
+variable "k8s_cluster_name" {
+	type = string
+	default = "tf-gke-cluster"
+}
+
+variable "k8s_cluster_location" {
+	type = string
+	default = "us-central1-a"
+}
+
+variable "k8s_remove_default_node_pool" {
+	type = bool
+	default = true
+}
+
+variable "k8s_initial_node_count" {
+	type = number
+	default = 1
+}
+
+variable "k8s_username" {
+	type = string
+	default = "clusterusername"
+}
+
+variable "k8s_password" {
+	type = string
+	default = "clusterspassword"
+}
+
+variable "k8s_issue_client_certificate" {
+	type = bool
+	default = false
+}
+
+variable "k8s_pool_name" {
+	type = string
+	default = "tf-node-pool"
+}
+
+variable "k8s_pool_location" {
+	type = string
+	default = "us-central1-a"
+}
+
+variable "k8s_pool_node_count" {
+	type = number
+	default = 1
+}
+
+variable "k8s_pool_preemptible" {
+	type = bool
+	default = true
+}
+
+variable "k8s_pool_machine_type" {
+	type = string
+	default = "e2-micro"
+}
+
+variable "k8s_pool_disable-legacy-endpoints" {
+	type = bool
+	default = true
+}
+
+variable "k8s_pool_oauth_scopes" {
+	type = list(string)
+	default = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+}
