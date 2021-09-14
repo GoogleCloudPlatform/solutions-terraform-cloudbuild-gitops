d828eeac5be22017e9b8094975235655b51844e2
monika16p <49428322+monika16p@users.noreply.github.com>
2021-09-14 17:45:16 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,20 +21,26 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
+
+
+module "kubernetes_engine" {
+	source = "./modules/kubernetes_engine"
+	count = var.kubernetes_engine-create ? var.kubernetes_engine-count : 0
+	k8s_cluster_name 		    = var.k8s_cluster_name
+	k8s_cluster_location 	  = var.k8s_cluster_location
+	k8s_remove_default_node_pool = var.k8s_remove_default_node_pool
+	k8s_initial_node_count 	= var.k8s_initial_node_count
+	k8s_username 			      = var.k8s_username
+	k8s_password 			      = var.k8s_password
+	k8s_issue_client_certificate = var.k8s_issue_client_certificate
+	k8s_pool_name 			    = var.k8s_pool_name
+	k8s_pool_location 		  = var.k8s_pool_location
+	k8s_pool_node_count 	  = var.k8s_pool_node_count
+	k8s_pool_preemptible 	  = var.k8s_pool_preemptible
+	k8s_pool_machine_type 	= var.k8s_pool_machine_type
+	k8s_pool_disable-legacy-endpoints = var.k8s_pool_disable-legacy-endpoints
+	k8s_pool_oauth_scopes 	= var.k8s_pool_oauth_scopes
   project = "${var.project}"
   env     = "${local.env}"
 }
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
