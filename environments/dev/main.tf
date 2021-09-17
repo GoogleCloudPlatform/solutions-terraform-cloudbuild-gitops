e24e15b028c6cdf1f689e4e7695b665394e412d1
monika16p <49428322+monika16p@users.noreply.github.com>
2021-09-17 12:05:27 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -40,7 +40,6 @@ module "kubernetes_engine" {
 	k8s_pool_machine_type 	= var.k8s_pool_machine_type
 	k8s_pool_disable-legacy-endpoints = var.k8s_pool_disable-legacy-endpoints
 	k8s_pool_oauth_scopes 	= var.k8s_pool_oauth_scopes
-	cluster_autoscaling = var.cluster_autoscaling
 	k8s_min_node_count = var.k8s_min_node_count
 	k8s_max_node_count = var.k8s_max_node_count
   	project = "${var.project}"
