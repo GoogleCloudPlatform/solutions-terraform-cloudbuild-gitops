5132250c3e0a10e8496d6d4f0d6a00569a3d8b59
Gilley <sean.gilley@outlook.com>
2021-10-12 16:10:57 +0100

    initial commit

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "tf-test-328715-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="tf-test-328715"
