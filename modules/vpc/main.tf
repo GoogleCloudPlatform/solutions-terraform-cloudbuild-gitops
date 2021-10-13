63c0ef56afb147ef69da6af946e3bec7c6cc41e5
Sanjeev Garikapati <sgarikapati@heb.ca>
2021-10-13 19:52:10 +0000

    Update project id and buckets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,7 +24,7 @@ module "vpc" {
     {
       subnet_name   = "${var.env}-subnet-01"
       subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = "us-west1"
+      subnet_region = "northamerica-northeast2"
     },
   ]
 
