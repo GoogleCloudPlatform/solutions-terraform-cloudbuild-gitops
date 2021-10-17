7b49bb655fa550a7a9d6172401e8f839a004c481
Jennifer-Stark <jastark1@gmail.com>
2021-10-17 11:12:47 +0000

    update project ids and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "silver-antonym-326607-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="silver-antonym-326607"
