12456aa998f6ee269f715d63dfb5a4c12a80d4c5
likhithmucherla <likhithmucherla98@gmail.com>
2021-10-25 14:26:31 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "kubernetes-monitoring-315412-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="kubernetes-monitoring-315412"
