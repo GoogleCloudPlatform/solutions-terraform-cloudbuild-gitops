435d1a9366305a6e91916a4d707c7a6a5a3930ba
rui-s-costa <89304646+rui-s-costa@users.noreply.github.com>
2021-10-27 17:11:26 -0400

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -40,19 +40,19 @@ module "firewall" {
 }
   
 
-resource "google_compute_instance" "default" {
- name         = "my-terraform-instance"
- machine_type = "n1-standard-1"
- zone         = "us-west1-a"
+# resource "google_compute_instance" "default" {
+#  name         = "my-terraform-instance"
+#  machine_type = "n1-standard-1"
+#  zone         = "us-west1-a"
 
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-9"
+#    }
+#  }
 
-  network_interface {
-   network = "default"
- }
+#   network_interface {
+#    network = "default"
+#  }
 
-}
+# }
