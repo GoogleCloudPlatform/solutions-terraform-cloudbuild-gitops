000b9ced8ed1182becebcd5a9c58fb20bb5be9f3
Fabiano Lira <fcstlira@gmail.com>
2021-11-01 17:06:09 +0000

    Atualizacao do project id

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "iac-demo-330816-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="iac-demo-330816"
