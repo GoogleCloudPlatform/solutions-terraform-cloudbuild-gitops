3c5efaf5f20857f6ffd840cfc6d781d4eb1aecf3
Sandeep Agarwal <pensande@gmail.com>
2021-11-14 17:45:24 +0000

    Named it as my server

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "http_server" {
   name         = "${local.network}-apache2-instance"
   machine_type = "f1-micro"
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Sandeep's Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
 
   boot_disk {
     initialize_params {
