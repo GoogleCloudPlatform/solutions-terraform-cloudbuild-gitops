c6a53473c143981a68b21d1f5615c698b9e49210
Sandeep Agarwal <pensande@gmail.com>
2021-11-16 14:16:30 +0000

    fixing instance template update policy

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -35,7 +35,5 @@ resource "google_compute_region_instance_group_manager" "webserver" {
     type                         = "PROACTIVE"
     instance_redistribution_type = "PROACTIVE"
     minimal_action               = "REPLACE"
-    max_surge_fixed              = 3
-    replacement_method           = "RECREATE"
   }
 }
