1df5ec7e4db7bb7022f6f03e17d0b31fb648f386
jsgarciaz <jsgarciaz@liverpool.com.mx>
2021-12-22 18:37:53 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "crp-dev-iac-testing-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="crp-dev-iac-testing"
