ff94f3dabf3439f0eeece3d4da5a4a0fc0540b10
jsgarciaz <55813790+jsgarciaz@users.noreply.github.com>
2021-12-22 10:50:49 -0600

    elimina resources

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,20 +21,20 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
+#module "vpc" {
+#  source  = "../../modules/vpc"
+#  project = "${var.project}"
+#  env     = "${local.env}"
+#}
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+#module "http_server" {
+#  source  = "../../modules/http_server"
+#  project = "${var.project}"
+#  subnet  = "${module.vpc.subnet}"
+#}
 
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+#module "firewall" {
+#  source  = "../../modules/firewall"
+#  project = "${var.project}"
+#  subnet  = "${module.vpc.subnet}"
+#}
