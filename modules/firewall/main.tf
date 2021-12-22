fac05cd835755b3486cf757232d5eacb9bd0db8a
jsgarciaz <55813790+jsgarciaz@users.noreply.github.com>
2021-12-22 10:44:54 -0600

    Update main.tf
    test

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -17,16 +17,16 @@ locals {
   network = "${element(split("-", var.subnet), 0)}"
 }
 
-#resource "google_compute_firewall" "allow-http" {
-#  name    = "${local.network}-allow-http"
-#  network = "${local.network}"
-#  project = "${var.project}"
+resource "google_compute_firewall" "allow-http" {
+  name    = "${local.network}-allow-http"
+  network = "${local.network}"
+  project = "${var.project}"
 
-#  allow {
-#    protocol = "tcp"
-#    ports    = ["80"]
-#  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
-#  target_tags   = ["http-server"]
-#  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+  source_ranges = ["0.0.0.0/0"]
 }
