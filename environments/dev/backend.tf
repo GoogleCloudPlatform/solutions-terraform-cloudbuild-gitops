f885655e7ecd49aa0e5bc43759e01e73848d3a79
ajinkyamore09oct <ajinkyamore09oct@gmail.com>
2021-12-23 14:16:41 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "idyllic-psyche-333315-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="idyllic-psyche-333315"
