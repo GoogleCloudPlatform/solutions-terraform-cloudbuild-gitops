0841e1201f0082fa0eefe4e86c4f2d1702a0981c
rui-s-costa <89304646+rui-s-costa@users.noreply.github.com>
2022-01-07 10:55:19 -0500

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -42,3 +42,29 @@ resource "google_compute_instance" "http_server" {
   # Apply the firewall rule to allow external IPs to access this instance
   tags = ["http-server"]
 }
+
+resource "google_compute_instance" "http_server" {
+  project      = "${var.project}"
+  zone         = "us-west1-a"
+  name         = "${local.network}-apache3-instance"
+  machine_type = "f1-micro"
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${var.subnet}"
+
+    access_config {
+      # Include this section to give the VM an external ip address
+    }
+  }
+
+  # Apply the firewall rule to allow external IPs to access this instance
+  tags = ["http-server"]
+}
