58df3bca6af83a554c5eccb2a9d843a55185a525
clebernandi <cleber.nandi@gmail.com>
2022-01-10 15:13:40 -0400

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "-facebook-capi-dev-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="-facebook-capi-dev"
