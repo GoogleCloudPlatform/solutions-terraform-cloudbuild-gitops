c1d26a3d83fb67d865e5143d6c37ce1c604f01ed
clebernandi <cleber.nandi@gmail.com>
2022-01-11 12:15:03 -0400

    add new resources

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,6 +21,52 @@ provider "google" {
   project = "${var.project}"
 }
 
+resource "google_project_service" "workflows" {
+  service            = "workflows.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_service_account" "workflows_service_account" {
+  account_id   = "sample-workflows-sa"
+  display_name = "Sample Workflows Service Account"
+}
+
+resource "google_workflows_workflow" "workflows_example" {
+  name            = "sample-workflow"
+  region          = "us-central1"
+  description     = "A sample workflow"
+  service_account = google_service_account.workflows_service_account.id
+  source_contents = <<-EOF
+  # This is a sample workflow, feel free to replace it with your source code
+  #
+  # This workflow does the following:
+  # - reads current time and date information from an external API and stores
+  #   the response in CurrentDateTime variable
+  # - retrieves a list of Wikipedia articles related to the day of the week
+  #   from CurrentDateTime
+  # - returns the list of articles as an output of the workflow
+  # FYI, In terraform you need to escape the $$ or it will cause errors.
+
+  - getCurrentTime:
+      call: http.get
+      args:
+          url: https://us-central1-workflowsample.cloudfunctions.net/datetime
+      result: CurrentDateTime
+  - readWikipedia:
+      call: http.get
+      args:
+          url: https://en.wikipedia.org/w/api.php
+          query:
+              action: opensearch
+              search: $${CurrentDateTime.body.dayOfTheWeek}
+      result: WikiResult
+  - returnOutput:
+      return: $${WikiResult.body[1]}
+EOF
+
+  depends_on = [google_project_service.workflows]
+}
+
 module "vpc" {
   source  = "../../modules/vpc"
   project = "${var.project}"
