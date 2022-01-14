0ab3b1e2c4e5fb084eaa567c5957cd3e9d162cee
clebernandi <cleber.nandi@gmail.com>
2022-01-14 16:36:36 -0400

    adjust main.tf file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -51,15 +51,15 @@ resource "google_cloudfunctions_function" "function" {
    }
 }
 
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.function.project
-  region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
+# # IAM entry for all users to invoke the function
+# resource "google_cloudfunctions_function_iam_member" "invoker" {
+#   project        = google_cloudfunctions_function.function.project
+#   region         = google_cloudfunctions_function.function.region
+#   cloud_function = google_cloudfunctions_function.function.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "cleber.nandi@dp6.com.br"
-}
+#   role   = "roles/cloudfunctions.invoker"
+#   member = "cleber.nandi@dp6.com.br"
+# }
 
 resource "google_project_service" "workflows" {
   service            = "workflows.googleapis.com"
