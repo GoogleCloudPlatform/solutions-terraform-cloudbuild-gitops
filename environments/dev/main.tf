d3bd9db2c6ca1b104ccc7e8c8f3d91cb8ea57a25
clebernandi <cleber.nandi@gmail.com>
2022-01-14 16:25:58 -0400

    adjust main.tf file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -41,7 +41,7 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb   = 256
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
-  trigger_http          = false
+  trigger_topic          = true
   entry_point           = "copy_table_capi"
 }
 
