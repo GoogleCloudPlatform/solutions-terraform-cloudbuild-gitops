280d5f417e28e5ad7300036a97c61727740f1b94
clebernandi <cleber.nandi@gmail.com>
2022-01-14 14:41:59 -0400

    Add resourses for create cloud functions and bucket

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,6 +21,39 @@ provider "google" {
   project = "${var.project}"
 }
 
+resource "google_storage_bucket" "bucket" {
+  name     = "dp6-bucket-capi"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "/root/workspace/dp6-facebook-capi-infra/assets"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "copy_table_capi"
+  description = "Copy table of the project Horizon to CAPI"
+  runtime     = "python38"
+
+  available_memory_mb   = 256
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  # trigger_http          = true
+  entry_point           = "copy_table_capi"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
 resource "google_project_service" "workflows" {
   service            = "workflows.googleapis.com"
   disable_on_destroy = false
