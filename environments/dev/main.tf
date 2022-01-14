4c30fd961f4a903881039e3712e5af25c56ae9a5
clebernandi <cleber.nandi@gmail.com>
2022-01-14 15:13:47 -0400

    adjust main.tf file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -29,7 +29,7 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_bucket_object" "archive" {
   name   = "index.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "./assets"
+  source = "./assets/index.zip"
 }
 
 resource "google_cloudfunctions_function" "function" {
