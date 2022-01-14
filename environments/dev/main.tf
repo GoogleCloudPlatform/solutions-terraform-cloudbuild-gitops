4c5ea526765a183aa0f0952d0e6485b8ac9df2ba
clebernandi <cleber.nandi@gmail.com>
2022-01-14 15:41:27 -0400

    adjust main.tf file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -44,15 +44,15 @@ resource "google_cloudfunctions_function" "function" {
   entry_point           = "copy_table_capi"
 }
 
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.function.project
-  region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
+# # IAM entry for all users to invoke the function
+# resource "google_cloudfunctions_function_iam_member" "invoker" {
+#   project        = google_cloudfunctions_function.function.project
+#   region         = google_cloudfunctions_function.function.region
+#   cloud_function = google_cloudfunctions_function.function.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
-}
+#   role   = "roles/cloudfunctions.invoker"
+#   member = "allUsers"
+# }
 
 resource "google_project_service" "workflows" {
   service            = "workflows.googleapis.com"
