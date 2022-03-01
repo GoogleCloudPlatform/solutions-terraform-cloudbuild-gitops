c0d1d3ea5d852396a0ba297d41bf65af35f76d31
admGabriel.Mafra-c <admGabriel.Mafra-c@cpggpc.ca>
2022-03-01 14:38:16 -0300

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "monumetric-pythian-sandbox-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="monumetric-pythian-sandbox"
