4173ac14af139ef57958edd918f9fb58c54199f1
GetItDone4 <99099484+GetItDone4@users.noreply.github.com>
2022-03-30 14:57:06 -0500

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -41,6 +41,6 @@ resource "google_compute_instance" "http_server" {
   }
 
   # Apply the firewall rule to allow external IPs to access this instance
-  # wvdg - force test #3
+  # wvdg - force test #4
   tags = ["http-server"]
 }
