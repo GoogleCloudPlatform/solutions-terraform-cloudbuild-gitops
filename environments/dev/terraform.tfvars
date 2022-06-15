f669baf0588a3c496f31e6197712686b41167fdd
giolong <congantran@gmail.com>
2022-06-15 00:50:26 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="neo2project"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "neo-bucket-tfstate"
     prefix = "env/prod"
   }
 }
