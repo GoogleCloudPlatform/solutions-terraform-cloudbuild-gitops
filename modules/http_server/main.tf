38c0ecead282b9757b470b667aad144dd97bb766
Kushal SURESH Dhone <65566260+kdhone24011996@users.noreply.github.com>
2022-06-23 13:27:26 +0530

    Update main.tf

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -19,7 +19,7 @@ locals {
 
 resource "google_compute_instance" "http_server" {
   project      = "${var.project}"
-  zone         = "us-east1"
+  zone         = "us-central1-a"
   name         = "${local.network}-apache2-instance"
   machine_type = "f1-micro"
 
