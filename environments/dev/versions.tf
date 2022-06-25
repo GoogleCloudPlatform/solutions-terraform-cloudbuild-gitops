c6110c51f86377145e1c836453a581da490c4937
kasjayatissa <kjayatissa@kpmg.com.au>
2022-06-25 21:00:21 +1000

    removed modules

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -14,5 +14,11 @@
 
 
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.50"
+    }
+  }
 }
