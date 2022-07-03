10edffed16557a0251d2dc0a4133bd0688d285c2
Sandeep Agarwal <pensande@gmail.com>
2022-07-03 17:12:23 +0000

    destroying prod app stack

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+/*
 output "external_ip" {
   value = "${module.http_lb.external_ip}"
 }
+*/
