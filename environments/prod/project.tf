f339f2923731b309421adf4e7eb1fdc8c5bbb331
Sandeep Agarwal <pensande@gmail.com>
2022-07-03 10:08:59 +0000

    enabling resource manager api

--- environments/prod/project.tf
+++ environments/prod/project.tf
@@ -8,3 +8,8 @@ resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "resourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
