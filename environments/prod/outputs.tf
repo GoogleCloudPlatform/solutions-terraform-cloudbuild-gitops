9d673bf165b0e2c7091309ce1f58ff91f17b78cb
Sandeep Agarwal <pensande@gmail.com>
2022-07-03 06:55:00 +0000

    destroy prod app-stack

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+/*
 output "external_ip" {
   value = "${module.load_balancer.external_ip}"
 }
+*/
