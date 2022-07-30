a17a307eede3f45913fecc8edb52d70b8f78368f
Sandeep Agarwal <pensande@gmail.com>
2022-07-30 17:30:16 +0000

    making cloud function updates deployable via terraform

--- modules/cloud_function/main.tf
+++ modules/cloud_function/main.tf
@@ -1,14 +1,18 @@
+locals {
+  cf_zip_archive_name = "cf-${var.function-name}-${data.archive_file.cf_source_zip.output_sha}.zip"
+}
+
 data "archive_file" "cf_source_zip" {
   type        = "zip"
   source_dir  = "../../functions/${var.function-name}"
   output_path = "${path.module}/tmp/${var.function-name}.zip"
 }
 
-resource "google_storage_bucket_object" "archive" {
-  name          = "${var.function-name}-index.zip"
-  bucket        = "${var.project}-source-code"
+resource "google_storage_bucket_object" "cf_source_zip" {
+  name          = local.cf_zip_archive_name
   source        = data.archive_file.cf_source_zip.output_path
   content_type  = "application/zip"
+  bucket        = "${var.project}-source-code"
 }
 
 resource "google_cloudfunctions_function" "function" {
@@ -19,7 +23,7 @@ resource "google_cloudfunctions_function" "function" {
   runtime     = "python39"
 
   source_archive_bucket = "${var.project}-source-code"
-  source_archive_object = google_storage_bucket_object.archive.name
+  source_archive_object = google_storage_bucket_object.cf_source_zip.name
   trigger_http          = true
   ingress_settings      = "ALLOW_ALL"
   entry_point           = var.entry-point
