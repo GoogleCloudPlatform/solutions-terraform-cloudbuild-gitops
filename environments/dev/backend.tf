849d9b03be0471ec3f4ff70cacc7f778e21faa4f
srirena3 <srinivas.shamkura@sada.com>
2022-08-02 23:22:13 -0500

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "sada-srini-tf-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="sada-srini-tf"
