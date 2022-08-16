1107e42a9c4e8c4c7beb35d9a95f5c4ba30e643f
dev-comsoft <m.sadiq@comsoftltd.com>
2022-08-16 16:49:07 +0000

    Update project Ids and backend

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "cabsol-234013-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="cabsol-234013"
