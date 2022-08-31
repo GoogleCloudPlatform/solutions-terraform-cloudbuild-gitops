5682ca65a41d347e7cbeff1c007dacd92435c7cb
clovethrowd916 <carl.lovejr@gmail.com>
2022-08-31 22:25:51 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="endless-terra-360719"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "endless-terra-360719-tfstate"
     prefix = "env/prod"
   }
 }
