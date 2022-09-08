0333152988ade986470de6a229ed756da64e841e
kensho7by <yatabe.hiroki.7by@creare-bzdz-dev.com>
2022-09-08 08:13:18 +0000

    change tags

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -40,5 +40,5 @@ resource "google_compute_instance" "http_server" {
   }
 
   # Apply the firewall rule to allow external IPs to access this instance
-  tags = ["http-server"]
+  tags = ["http-server","dev"]
 }
