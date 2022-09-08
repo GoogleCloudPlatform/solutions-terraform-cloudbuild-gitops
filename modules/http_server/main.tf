43fea0f8d9cbc099d8fe2c51f298590d876bd34d
kensho7by <yatabe.hiroki.7by@creare-bzdz-dev.com>
2022-09-08 07:56:49 +0000

    change instance name

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -20,7 +20,7 @@ locals {
 resource "google_compute_instance" "http_server" {
   project      = "${var.project}"
   zone         = "us-west1-a"
-  name         = "${local.network}-apache2-instance"
+  name         = "${local.network}-apache2-instance-hy"
   machine_type = "f1-micro"
 
   metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
