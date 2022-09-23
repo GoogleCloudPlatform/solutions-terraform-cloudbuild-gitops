52348aa2516425dffa613cf9e1dc96b95aa2b0d4
nyucragavilan@gmail.com <nilofe>
2022-09-22 23:54:07 -0500

    update id buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "kubernets-test-loki-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="kubernets-test-loki"
