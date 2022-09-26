f2ffb4c9bdcab8c498876ea5a0822ef1c4fd928e
Sandeep Agarwal <pensande@gmail.com>
2022-09-26 17:14:33 +0000

    enabling private_ip_access in subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,9 +22,10 @@ module "vpc" {
 
   subnets = [
     {
-      subnet_name   = "${var.env}-subnet-01"
-      subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = var.region
+      subnet_name           = "${var.env}-subnet-01"
+      subnet_ip             = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
+      subnet_region         = var.region
+      subnet_private_access = "true"
     },
   ]
 
