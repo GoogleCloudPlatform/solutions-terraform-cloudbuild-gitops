39f3788ee5217864d07c2f33799aed1e5fa3e8cd
Sandeep Agarwal <pensande@gmail.com>
2022-09-26 12:45:23 +0000

    enabling gke service in the project

--- environments/dev/project.tf
+++ environments/dev/project.tf
@@ -1,20 +1,10 @@
 # Enable services
 resource "google_project_service" "compute" {
+  project = var.project
   service = "compute.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "iam" {
-  service = "iam.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "resourcemanager" {
-  service = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
 }
 
 resource "google_project_service" "managed_kubernetes" {
+  project = var.project
   service = "container.googleapis.com"
-  disable_on_destroy = false
 }
