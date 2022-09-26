91d11dfb30e9869a92add1703bd9a8345fbeff71
Sandeep Agarwal <pensande@gmail.com>
2022-09-26 11:54:59 +0000

    enabling services in project

---
+++ environments/dev/project.tf
@@ -0,0 +1,20 @@
+# Enable services
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "resourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "managed_kubernetes" {
+  service = "container.googleapis.com"
+  disable_on_destroy = false
+}
