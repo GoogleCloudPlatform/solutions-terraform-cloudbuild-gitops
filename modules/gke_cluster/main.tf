2b618f6f4c24e4639ebe332760f18bfb2c4e0137
Sandeep Agarwal <pensande@gmail.com>
2022-09-26 09:42:37 +0000

    fixing bugs in gke_cluster module

--- modules/gke_cluster/main.tf
+++ modules/gke_cluster/main.tf
@@ -1,15 +1,22 @@
-resource "google_container_cluster" "primary" {
+resource "google_container_cluster" "cluster" {
   name                      = var.cluster_name
   description               = "terraform-created gke cluster"
   location                  = var.region
   network                   = var.network
   subnetwork                = var.subnetwork
+  initial_node_count        = 3
   enable_shielded_nodes     = true
-  enable_secure_boot        = true
-  enable_private_nodes      = true
-  enable_private_endpoint   = false
-
-  initial_node_count = 3
+  
+  private_cluster_config {
+    enable_private_nodes      = true
+    enable_private_endpoint   = false
+  
+  }
+  
+  shielded_instance_config {
+    enable_integrity_monitoring = true 
+    enable_secure_boot          = false
+  }
 
   timeouts {
     create = "30m"
@@ -17,10 +24,10 @@ resource "google_container_cluster" "primary" {
   }
 
   ip_allocation_policy {
-      cluster_ipv4_cidr_block   = var.master_ipv4_cidr
+    cluster_ipv4_cidr_block   = var.master_ipv4_cidr
   }
 
   binary_authorization {
-      evaluation_mode           = "PROJECT_SINGLETON_POLICY_ENFORCE"
+    evaluation_mode           = "PROJECT_SINGLETON_POLICY_ENFORCE"
   }
 }
