47b055fd3c7465d9f18f709936c0750160134f8f
Sandeep Agarwal <pensande@gmail.com>
2022-09-30 13:45:24 +0000

    updates to cloud function code

--- functions/deploy-notification/main.py
+++ functions/deploy-notification/main.py
@@ -12,15 +12,15 @@ def deploy_notification(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     message_json = json.loads(pubsub_message)
     
-    send_slack_chat_notification(message_json['finding'], message_json['resource'])
+    send_slack_chat_notification(message_json['message']['attributes'])
 
-def send_slack_chat_notification(finding_json, resource_json):
-    slack_message = [ 
+def send_slack_chat_notification(operations_json):
+    slack_message = [
             {
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": f"{finding_json['severity']} severity finding {finding_json['category']} detected!"
+                    "text": f"Cloud Deploy Operation Alert!"
                 }
             },
             {
@@ -28,33 +28,14 @@ def send_slack_chat_notification(finding_json, resource_json):
             },
             {
                 "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"{finding_json['description']}\n"
-                }
-            },
-            {
-                "type": "section",
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": f"*Project:*\n{resource_json['projectDisplayName']}"
+                        "text": f"*Action:*\n{operations_json['Action']}"
                     },
                     {
                         "type": "mrkdwn",
-                        "text": f"*When:*\n{finding_json['eventTime']} UTC"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Resource Type:*\n{resource_json['type']}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Resource Name:*\n{resource_json['displayName']}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Resource URI:*\n{finding_json['externalUri']}"
+                        "text": f"*PipelineID:*\n{operations_json['DeliveryPipelineId']}"
                     }
                 ]
             }
@@ -65,7 +46,7 @@ def send_slack_chat_notification(finding_json, resource_json):
         response = requests.post("https://slack.com/api/chat.postMessage", data={
             "token": slack_token,
             "channel": slack_channel,
-            "text": f"{finding_json['severity']} severity finding {finding_json['category']} detected!",
+            "text": f"{operations_json['Action']} in {operations_json['DeliveryPipelineId']} reported!",
             "blocks": json.dumps(slack_message)
         })
         print(f"Slack responded with Status Code: {response.status_code}")
