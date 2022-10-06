2f88c6ed4fe1054e456cc5d242bd79fe3506e9f3
Olav Nymoen <olav.nymoen@finn.no>
2022-10-06 15:46:06 +0200

    versioning

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -21,24 +21,6 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
 module "workbench" {
   source  = "../../modules/workbench"
   project = "${var.project}"
