779a3c2983cc4a4edb7dc3fc1c170ad4e82272cb
Olav Nymoen <olav.nymoen@finn.no>
2022-10-07 11:53:56 +0200

    wrong out path

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -61,10 +61,10 @@ module "hello_world_pipeline" {
   cloud_scheduler_region = "europe-west1"
   pipeline_spec_path = "gs://df-data-science-test-pipelines/prod/basic_pipeline.json"
   parameter_values = {
-    "a" = "stres"
-    "b" = "sed"
+    "a" = "kake"
+    "b" = "bake"
   }
-  gcs_output_directory = "gs://df-data-science-test/prod/out/"
+  gcs_output_directory = "gs://df-data-science-test-pipelines/prod/out/"
   vertex_service_account_email = "364866568815-compute@developer.gserviceaccount.com"
   time_zone                    = "UTC"
   schedule                     = "0 0 * * *"
