80290872d39447427b73f1a2a96339f311d6c941
Olav Nymoen <olav.nymoen@finn.no>
2022-10-12 15:05:43 +0200

    try adding cloud function

--- modules/model/main.tf
+++ modules/model/main.tf
@@ -1,4 +1,4 @@
-# Workbench instance
+# Creates the workbench instance
 
 resource "google_notebooks_instance" "main" {
   name                  = var.model_name
@@ -23,4 +23,41 @@ resource "google_notebooks_instance" "main" {
   metadata = {
     terraform = "true"
   }
+}
+
+
+resource "google_storage_bucket" "function_bucket" {
+    name     = "${var.project}-function"
+    location = "europe-west4"
+}
+
+# Generates an archive of the source code compressed as a .zip file.
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "../../cloud_fun_src"
+  output_path = "/tmp/function.zip"
+}
+
+# Add source code zip to the Cloud Function's bucket
+resource "google_storage_bucket_object" "zip" {
+  source        = data.archive_file.source.output_path
+  content_type  = "application/zip"
+
+  # Append to the MD5 checksum of the files's content
+  # to force the zip to be updated as soon as a change occurs
+  name          = "src-${data.archive_file.source.output_md5}.zip"
+  bucket        = google_storage_bucket.function_bucket.name
+}
+
+# Create the Cloud function triggered by a `Finalize` event on the bucket
+resource "google_cloudfunctions_function" "function" {
+  name                  = "function-trigger-on-gcs"
+  runtime               = "python37"
+
+  # Get the source code of the cloud function as a Zip compression
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
+
+  # Must match the function name in the cloud function `main.py` source code
+  trigger_http          = true
 }
