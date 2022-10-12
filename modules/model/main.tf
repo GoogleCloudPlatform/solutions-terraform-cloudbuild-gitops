b6f9649f6d8d77ad14a5fcdf13010d94a2eea420
Olav Nymoen <olav.nymoen@finn.no>
2022-10-12 13:27:30 +0200

    try conditional block

--- modules/model/main.tf
+++ modules/model/main.tf
@@ -6,10 +6,15 @@ resource "google_notebooks_instance" "main" {
   location              = "europe-west4-a"
   machine_type          = var.machine_type
   install_gpu_driver    = var.gpu_count == 0 ? false : true
-  accelerator_config    = var.gpu_count == 0 ? null : {
-    type                = var.gpu_type
-    core_count          = var.gpu_count
+
+  dynamic "accelerator_config" {
+    for_each = var.gpu_count == 0 ? [] : [1]
+    content {
+      type             = var.gpu_type
+      core_count       = var.gpu_count
+    }
   }
+
   instance_owners       = var.instance_owners
   container_image {
     repository  = "gcr.io/deeplearning-platform-release/tf2-gpu.2-10"
