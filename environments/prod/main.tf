349ab4b552b4c7db2ccbc86afa2d2981ec21e327
Olav Nymoen <olav.nymoen@finn.no>
2022-10-13 22:17:34 +0200

    iterating over models

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -15,6 +15,11 @@
 
 locals {
   env = "prod"
+
+  model_config_files = fileset(path.module, "models/*/config.yaml")
+
+  model_configs = { for config_file in local.model_config_files:
+                    config_file => yamldecode(file(config_file))}
 }
 
 provider "google" {
@@ -24,7 +29,10 @@ provider "google" {
 // Models TODO: iterate over yaml config files in folders
 module "model" {
   source            = "../../modules/model"
-  model_name        = "module-model-test"
+
+  for_each          = local.model_configs
+
+  model_name        = each.value.model_name
   project           = var.project
   gpu_count         = 1
   pipeline_endpoint = google_cloud_run_service.scheduler.status[0].url
@@ -93,129 +101,3 @@ resource "google_cloudbuild_trigger" "pipeline" {
     }
   }
 }
-
-
-/*
-module "workbench" {
-  source  = "../../modules/workbench"
-  project = "${var.project}"
-}
-
-resource "google_storage_bucket" "auto-expire" {
-  name          = "df-data-science-test-pipelines"
-  location      = "europe-west4"
-  force_destroy = true
-
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
-}
-
-#module "working_pipeline" {
-#  source = "teamdatatonic/scheduled-vertex-pipelines/google"
-#  project = "${var.project}"
-#  vertex_region = "europe-west4"
-#  cloud_scheduler_region = "europe-west1"
-#  pipeline_spec_path = "gs://df-data-science-test-pipelines/prod/pipeline.json"
-#  parameter_values = {}
-#  gcs_output_directory = "gs://df-data-science-test-pipelines/prod/out/"
-#  vertex_service_account_email = "364866568815-compute@developer.gserviceaccount.com"
-#  time_zone                    = "UTC"
-#  schedule                     = "0 0 * * *"
-#  cloud_scheduler_job_name     = "working-pipeline-schedule"
-#}
-#
-#module "not_working_pipeline" {
-#  source = "teamdatatonic/scheduled-vertex-pipelines/google"
-#  project = "${var.project}"
-#  vertex_region = "europe-west4"
-#  cloud_scheduler_region = "europe-west1"
-#  pipeline_spec_path = "gs://df-data-science-test-pipelines/prod/pipeline3.json"
-#  parameter_values = {
-#    "a" = "Hello, world!"
-#    "b" = "Hello, world!"
-#  }
-#  gcs_output_directory = "gs://df-data-science-test-pipelines/prod/out/"
-#  vertex_service_account_email = "364866568815-compute@developer.gserviceaccount.com"
-#  time_zone                    = "UTC"
-#  schedule                     = "0 0 * * *"
-#  cloud_scheduler_job_name     = "not-working-pipeline-schedule"
-#}
-
-
-#module "real_pipeline" {
-#  source = "teamdatatonic/scheduled-vertex-pipelines/google"
-#  project = "${var.project}"
-#  vertex_region = "europe-west4"
-#  cloud_scheduler_region = "europe-west1"
-#  pipeline_spec_path = "gs://df-data-science-test-pipelines/prod/pipeline.json"
-#  parameter_values = {
-#    "project" = "df-data-science-test"
-#  }
-#  gcs_output_directory = "gs://df-data-science-test-pipelines/prod/out/"
-#  vertex_service_account_email = "364866568815-compute@developer.gserviceaccount.com"
-#  time_zone                    = "UTC"
-#  schedule                     = "0 0 * * *"
-#  cloud_scheduler_job_name     = "real-pipeline-schedule"
-#}
-
-# Attempt our own implementation
-
-data "google_storage_bucket_object_content" "pipeline_spec" {
-  name   = "prod/pipeline.json"
-  bucket = "df-data-science-test-pipelines"
-}
-
-locals {
-  pipeline_spec = jsondecode(data.google_storage_bucket_object_content.pipeline_spec.content)
-
-  pipeline_job = {
-    displayName = "self-made-pipeline"
-    pipelineSpec = local.pipeline_spec
-    labels       = {}
-#    runtimeConfig = {
-#      parameterValues    = {
-#        # "text" = "hello world!"
-#        "a" = "a inp"
-#        "b" = "b inp"
-#      }
-#      gcsOutputDirectory = "gs://df-data-science-test-pipelines/prod/out/"
-#    }
-    encryptionSpec = null
-    serviceAccount = "364866568815-compute@developer.gserviceaccount.com"
-    # network        = var.network
-  }
-
-  merged_job = merge(local.pipeline_spec, local.pipeline_job)
-}
-
-resource "google_cloud_scheduler_job" "job" {
-  name = "self-made-job-reread"
-  project = "${var.project}"
-  description = "Our very own scheduled job"
-  schedule = "0 0 * * *" 
-  time_zone = "UTC"
-  attempt_deadline = "320s"
-  region = "europe-west1"
-
-  retry_config {
-    retry_count = 1
-  }
-
-  http_target {
-    http_method = "POST"
-    uri         = "https://europe-west4-aiplatform.googleapis.com/v1/projects/${var.project}/locations/europe-west4/pipelineJobs"
-    body        = base64encode(jsonencode(local.pipeline_spec))
-
-    oauth_token {
-      service_account_email = "364866568815-compute@developer.gserviceaccount.com"
-    }
-  }
-}
-
-*/
