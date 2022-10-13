6e7983fc03b43ef33a939c146eaf3a2f0f748d4f
Olav Nymoen <olav.nymoen@finn.no>
2022-10-13 15:17:42 +0200

    working service?

--- run/pipeline_scheduler/app/main.py
+++ run/pipeline_scheduler/app/main.py
@@ -1,11 +1,16 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
+from google import auth
+from google.auth.transport import urllib3
+from google.cloud import storage
 
 
 class Job(BaseModel):
     url: str
+    gcs_bucket: str
     gcs_pipeline: str
 
 app = FastAPI()
@@ -16,8 +21,26 @@ def read_root():
 
 @app.post('/')
 async def run_job(job: Job):
-    return job
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(job['gcs_bucket'])
+    blob = bucket.blob(job['gcs_pipeline'])
 
+    pipeline_job_description = blob.download_as_string()
+
+    credentials, _ = auth.default()
+    authorized_http = urllib3.AuthorizedHttp(credentials=credentials)
+    response = authorized_http.request(
+        url=job['url'],
+        method='POST',
+        body=pipeline_job_description,
+    )
+
+    if response.status != 200:
+        raise HTTPException(
+            status_code=response.status,
+            detail=response.data)
+
+    return response.data
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
