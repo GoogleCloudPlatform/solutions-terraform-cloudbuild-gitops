d28c5d282f35cd36b1eb35deef49778405d9c520
Olav Nymoen <olav.nymoen@finn.no>
2022-10-13 15:33:42 +0200

    k

--- run/pipeline_scheduler/app/main.py
+++ run/pipeline_scheduler/app/main.py
@@ -22,15 +22,15 @@ def read_root():
 @app.post('/')
 async def run_job(job: Job):
     storage_client = storage.Client()
-    bucket = storage_client.bucket(job['gcs_bucket'])
-    blob = bucket.blob(job['gcs_pipeline'])
+    bucket = storage_client.bucket(job.gcs_bucket)
+    blob = bucket.blob(job.gcs_pipeline)
 
     pipeline_job_description = blob.download_as_string()
 
     credentials, _ = auth.default()
     authorized_http = urllib3.AuthorizedHttp(credentials=credentials)
     response = authorized_http.request(
-        url=job['url'],
+        url=job.url,
         method='POST',
         body=pipeline_job_description,
     )
