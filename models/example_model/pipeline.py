ba094b0991855c4e21189b409a59152a97729547
Olav Nymoen <olav.nymoen@finn.no>
2022-10-19 14:32:13 +0200

    try auto make pipeline

---
+++ models/example_model/pipeline.py
@@ -0,0 +1,65 @@
+from kfp.v2 import compiler, dsl
+from kfp.v2.dsl import (Artifact,
+                        Dataset,
+                        Input,
+                        Model,
+                        Output,
+                        Metrics,
+                        ClassificationMetrics,
+                        component, 
+                        OutputPath, 
+                        InputPath)
+
+from google.cloud import aiplatform
+from google_cloud_pipeline_components import aiplatform as gcc_aip
+
+@component(
+    packages_to_install=["pandas", "fsspec", "gcsfs"],
+    base_image="python:3.9",
+)
+def get_dataset(
+    url: str,
+    train_ds: Output[Dataset],
+    test_ds: Output[Dataset],
+):
+    import pandas as pd
+
+    df = pd.read_csv('gs://df-data-science-test-data/vt_data.csv')
+    df.to_csv(train_ds.path + '.csv', index=False)
+
+
+
+@component(base_image="gcr.io/deeplearning-platform-release/tf2-gpu.2-10")
+def train(
+        dataset: Input[Dataset],
+        model: Output[Model]
+    ):
+
+    import tensorflow as tf
+    print(tf.__version__)
+    print(tf.config.list_physical_devices())
+
+    print(dataset)
+    print(model)
+
+@dsl.pipeline(
+    name='training-pipeline',
+    description='Learning to make a training pipeline',
+    pipeline_root="gs://df-data-science-test-pipelines/out")
+def pipeline(
+    url: str = "gs://df-data-science-test-data/vt_data.csv"
+):
+    data = get_dataset(url)
+
+    result = (train(
+            dataset=data.outputs["train_ds"]
+        ).
+        set_cpu_limit('4').
+        set_memory_limit('64G').
+        add_node_selector_constraint('cloud.google.com/gke-accelerator', 'NVIDIA_TESLA_T4').
+        set_gpu_limit('1'))
+
+    return result
+
+
+compiler.Compiler().compile(pipeline_func=pipeline, package_path='pipeline.json')
