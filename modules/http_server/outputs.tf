bf7c26b146b659dda10ab8de882a74fa761973a1
luctrate <stev.hooker@gmail.com>
2022-10-25 13:57:36 +0200

    fix outputs

--- modules/http_server/outputs.tf
+++ modules/http_server/outputs.tf
@@ -14,9 +14,9 @@
 
 
 output "instance_name" {
-  value = "${google_compute_instance.http_server.name}"
+  value = "${google_compute_instance[*].http_server.name}"
 }
 
 output "external_ip" {
-  value = "${google_compute_instance.http_server.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance[*].http_server.network_interface.0.access_config.0.nat_ip}"
 }
