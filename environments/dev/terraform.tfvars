5ca9c77e37638ffb4b2cc8248fd7a158dd6ae051
Red Daly <reddaly@gmail.com>
2022-11-08 00:17:52 -0800

    Updat project id per tutorial

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="red-gitops-experiment"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "red-gitops-experiment-tfstate"
     prefix = "env/prod"
   }
 }
