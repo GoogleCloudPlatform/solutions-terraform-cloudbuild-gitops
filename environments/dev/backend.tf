43b9164628683983e22074f127c08613b2aa690f
Timothy Mahoney <timothy.mahoney1@se-c02f169qmd6n.lan>
2022-12-08 14:29:21 +0100

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "ingka-itoi-explore-dev-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="ingka-itoi-explore-dev"
