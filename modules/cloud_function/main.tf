22658a17d1164107e57bc2fe78bf25abcab4e09a
Rekha Danasekaran <sivanesanprabhakar@gmail.com>
2023-01-17 23:25:03 -0500

    update

---
+++ modules/cloud_function/main.tf
@@ -0,0 +1,37 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+resource "google_storage_bucket" "bucket" {
+  name     = "cf-source-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "test-cf.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./CF_ZIPS"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "http-function"
+  description = "Http trigger test function"
+  runtime     = "python310"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "hello_world"
+}
