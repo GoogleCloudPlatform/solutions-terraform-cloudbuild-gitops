cef4c90af4be3912426805113328f5ed0fe3488b
Rekha Danasekaran <sivanesanprabhakar@gmail.com>
2023-01-17 23:43:17 -0500

    update

--- modules/cloud_function/main.tf
+++ modules/cloud_function/main.tf
@@ -21,7 +21,7 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_bucket_object" "archive" {
   name   = "test-cf.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "CF_ZIPS"
+  source = "${path.module}/CF_ZIPS/test-cf.zip"
 }
 
 resource "google_cloudfunctions_function" "function" {
