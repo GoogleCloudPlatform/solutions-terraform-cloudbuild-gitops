3aa89cdded54281f79db397b5c1b2ae60c30517f
Sandeep Agarwal <pensande@gmail.com>
2023-01-25 04:40:45 +0000

    destroying dev cluster

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -34,7 +34,7 @@ module "cloud_nat" {
   network = "${module.vpc.network}"
   region  = "${var.region}"
 }
-
+/*
 module "gke_cluster" {
     source          = "../../modules/gke_cluster"
     cluster_name    = "${local.env}-binauthz"
@@ -62,7 +62,7 @@ resource "google_project_iam_member" "compute_container_admin" {
   role     = "roles/container.admin"
   member   = "serviceAccount:${module.gke_cluster.service-account}"
 }
-
+*/
 # Artifact Registry repo for binauthz-demo
 resource "google_artifact_registry_repository" "binauth-demo-repo" {
   provider      = google-beta
