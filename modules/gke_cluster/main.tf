732d2571339093113048316d46ee157567a823bc
Sandeep Agarwal <pensande@gmail.com>
2023-01-25 04:16:31 +0000

    syntax error

--- modules/gke_cluster/main.tf
+++ modules/gke_cluster/main.tf
@@ -18,7 +18,7 @@ resource "google_container_cluster" "cluster" {
   enable_shielded_nodes         = true
   
   binary_authorization {
-      evaluation_mode = "PROJECT_SINGLETON_POLICY_ENFORCE"
+    evaluation_mode = "PROJECT_SINGLETON_POLICY_ENFORCE"
   }
   
   node_config {
