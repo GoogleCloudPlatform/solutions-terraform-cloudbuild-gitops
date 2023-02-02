0596ba317d61e49646a7af43c4fb35c44d27a2ac
Rishabh Dhakar <rdhakar@google.com>
2023-02-02 18:28:44 +0530

    one mistake

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   env     = "${local.env}"
 }
 
-module "http_server2" {
+module "http_server" {
   source  = "../../modules/http_server"
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
