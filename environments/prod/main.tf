d4777b3d55db872222e51b6241d9ac89e84308fc
Sandeep Agarwal <pensande@gmail.com>
2023-02-11 08:07:06 +0000

    reverting again from modules to resources

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -37,10 +37,23 @@ resource "google_storage_bucket" "bucket" {
 ## Secure CI/CD Binary Authorization Demo ##
 ############################################
 
-module "vpc" {
-  source  = "../../modules/vpc"
+resource "google_compute_network" "vpc" {
+  name                    = "${local.env}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name                      = "${local.env}-subnet-01"
+  ip_cidr_range             = "10.${local.env == "dev" ? 10 : 20}.0.0/24"
+  region                    = "${var.region}"
+  network                   = google_compute_network.vpc.id
+  private_ip_google_access  = true
+}
+
+module "cloud_nat" {
+  source  = "../../modules/cloud_nat"
   project = var.project
-  env     = local.env
+  network = google_compute_network.vpc.name
   region  = var.region
 }
 
@@ -48,8 +61,8 @@ module "gke_cluster" {
     source          = "../../modules/gke_cluster"
     cluster_name    = "${local.env}-binauthz"
     region          = var.region
-    network         = module.vpc.id
-    subnetwork      = module.vpc.subnet
+    network         = google_compute_network.vpc.id
+    subnetwork      = google_compute_subnetwork.subnet.id
     master_ipv4_cidr= "10.${local.env == "dev" ? 10 : 20}.1.16/28"
 }
 
