908230a575676892dac82544b8181b7128055bfb
jamice-labilles-telus <jamice.labilles@telusinternational.com>
2023-02-17 05:51:45 +0000

    update project ids and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "mds-tf2-lab-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="mds-tf2-lab"
