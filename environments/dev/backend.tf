dc6351fe848caba3aa583cd8b656817a6b455eee
Trey <treyspeakman@gmail.com>
2023-02-22 17:07:36 -0600

    Update project ids and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "solar-curve-378603-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="solar-curve-378603"
