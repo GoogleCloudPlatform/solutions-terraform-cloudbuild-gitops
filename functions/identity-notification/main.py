95000d6201bbb8dc6351d6700ccd60c05c270564
Sandeep Agarwal <pensande@gmail.com>
2023-03-28 08:33:44 +0000

    fixing bug in identity-notification function

--- functions/identity-notification/main.py
+++ functions/identity-notification/main.py
@@ -2,6 +2,8 @@ import os
 import base64
 import json
 import requests
+from pytz import timezone
+from datetime import datetime
 
 def identity_notification(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -13,58 +15,54 @@ def identity_notification(event, context):
     # """.format(context.event_id, context.timestamp, context.resource["name"]))
 
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    message_json = json.loads(pubsub_message)
+    event_json = json.loads(pubsub_message)
 
-    print(message_json)
-
-    if 'severity' in message_json:
-        try:
-            if message_json['severity'] == "NOTICE":
-                send_slack_chat_notification(message_json['protoPayload'])
-            else:
-                print("Ignoring message")
-        except Exception as e:
-            print(e)
+    if 'protoPayload' in event_json:
+        send_slack_chat_notification(event_json['protoPayload'])
     else:
         print("Missing data payload in function trigger event")
 
 def send_slack_chat_notification(message_json):
-    slack_message = [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": f"Cloud Identity Activity Alert!"
-                }
-            },
-            {
-                "type": "divider"
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*User:*\n{message_json['authenticationInfo']['principalEmail']}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Event:*\n{message_json['metadata']['event'][0]['eventName']}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Timestamp:*\n{message_json['metadata']['activityId']['timeUsec']}"
-                    }
-                ]
-            }
-        ]
     try:
+        posix_ts    = int(message_json['metadata']['activityId']['timeUsec'])/1000000
+        timestamp   = datetime.utcfromtimestamp(posix_ts).astimezone(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S')
+
+        slack_message = [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"Cloud Identity Activity Alert!"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*User:*\n{message_json['authenticationInfo']['principalEmail']}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Event:*\n{message_json['metadata']['event'][0]['eventName']}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Timestamp:*\n{timestamp} IST"
+                        }
+                    ]
+                }
+            ]
+    
         slack_token = os.environ.get('SLACK_ACCESS_TOKEN', 'Specified environment variable is not set.')
-        slack_channel = os.environ.get('SLACK_DEVOPS_CHANNEL', 'Specified environment variable is not set.')
+        slack_channel = os.environ.get('SLACK_CHANNEL', 'Specified environment variable is not set.')
         response = requests.post("https://slack.com/api/chat.postMessage", data={
             "token": slack_token,
             "channel": slack_channel,
-            "text": f"{message_json['authenticationInfo']['principalEmail']} performed {message_json['metadata']['event'][0]['eventName']}!",
+            "text": f"{message_json['authenticationInfo']['principalEmail']} performed {message_json['metadata']['event'][0]['eventName']} at {timestamp} IST!",
             "blocks": json.dumps(slack_message)
         })
         print(f"Slack responded with Status Code: {response.status_code}")
