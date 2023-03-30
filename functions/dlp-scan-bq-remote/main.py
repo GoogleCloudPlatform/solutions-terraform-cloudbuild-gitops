bce9d9f46a37b68899ab564d99ac8ea154db7e74
Sandeep Agarwal <pensande@gmail.com>
2023-03-30 17:08:29 +0000

    fixing bugs in dlp-scan-bq-remote function

--- functions/dlp-scan-bq-remote/main.py
+++ functions/dlp-scan-bq-remote/main.py
@@ -5,13 +5,13 @@ from google.cloud import dlp
 
 @functions_framework.http
 def dlp_scan_bq_remote(request):
-  # The infoTypes of information to match
-  INFO_TYPES = [
-    'PHONE_NUMBER', 'EMAIL_ADDRESS', 'US_SOCIAL_SECURITY_NUMBER'
-  ]
+  # create a dlp client for this project
   dlp_client = dlp.DlpServiceClient()
   PROJECT_NAME = os.environ.get('PROJECT_NAME')
   parent = f"projects/{PROJECT_NAME}"
+  
+  # The infoTypes of information to match
+  INFO_TYPES = ['PHONE_NUMBER', 'EMAIL_ADDRESS', 'US_SOCIAL_SECURITY_NUMBER']
   inspect_config = {"info_types": [{"name": info_type} for info_type in INFO_TYPES]}
   deidentify_config = {
     "info_type_transformations": {
@@ -32,15 +32,15 @@ def dlp_scan_bq_remote(request):
     request_json = request.get_json()
     calls = request_json['calls']
     for call in calls:
-      return_value.append(response_text = dlp_client.deidentify_content(
+      response_text = dlp_client.deidentify_content(
         request={
             "parent": parent,
             "deidentify_config": deidentify_config,
             "inspect_config": inspect_config,
-            "item": {"value": call},
+            "item": {"value": call[0]},
         }
-    ))
-    print(return_value)
+      )
+      return_value.append(response_text.item.value)
     return jsonify( { "replies":  return_value } )
   except Exception as e:
     return jsonify( { "errorMessage": str(e) } ), 400
