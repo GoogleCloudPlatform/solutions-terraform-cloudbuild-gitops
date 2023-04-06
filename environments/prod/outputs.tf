b2da7a493264c58210e42c024e87380a062d95eb
heikoliedtke <heikoliedtke@google.com>
2023-04-06 11:56:17 +0200

    fix

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -28,7 +28,3 @@ output "firewall_rule" {
 output "instance_name" {
   value = "${module.http_server.instance_name}"
 }
-
-output "external_ip" {
-  value = "${module.http_server.external_ip}"
-}
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="prj-terraform-369614"
