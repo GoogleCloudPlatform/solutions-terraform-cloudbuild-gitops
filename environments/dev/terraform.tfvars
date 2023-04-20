e52f425e2a0577d7273a40a4130ba79c7db57b62
praveendanda <danda.praveen@gmail.com>
2023-04-20 15:52:23 -0500

    Updated project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="onymos-dev-2"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "onymos-dev-2-tfstate"
     prefix = "env/prod"
   }
 }
