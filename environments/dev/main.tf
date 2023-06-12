b8566ed80234e53b6a67f78603b722f091b90132
heikoliedtke <heikoliedtke@google.com>
2023-06-12 16:04:14 +0200

    fix:

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,14 +27,14 @@ module "vpc" {
   env     = "${local.env}"
   region = "${var.region}"
 }
-
+/*
  module "http_server" {
   source  = "../../modules/http_server"
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
   region = "${var.region}"
 }
-
+*/
 module "firewall" {
   source  = "../../modules/firewall"
   project = "${var.project}"
