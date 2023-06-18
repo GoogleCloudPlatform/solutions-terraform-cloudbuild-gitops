c3dd33b5e7e0c7d0cb4312449f25a01dcd660b8d
Sandeep Agarwal <pensande@gmail.com>
2023-06-18 12:12:42 +0000

    commenting out maxscale annotation

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -467,7 +467,7 @@ resource "google_cloud_run_service" "iap_run_service" {
 
   metadata {
     annotations = {
-      "autoscaling.knative.dev/maxScale"      = "2"
+      #"autoscaling.knative.dev/maxScale"      = "2"
       "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.iap_run_sql_demo_db_instance.connection_name
       "run.googleapis.com/client-name"        = "terraform"
       "run.googleapis.com/ingress"            = "internal-and-cloud-load-balancing"
