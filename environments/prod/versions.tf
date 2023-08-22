f09b5312173de3bf8580d2dab1debd5d80ae2242
Nathan <nathan.he@acncloud.com.cn>
2023-08-22 17:51:22 +0800

    Update TF backend config settings

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# calshi
 #     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -14,5 +14,5 @@
 
 
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.5.0"
 }
