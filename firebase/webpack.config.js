7720ce2bf0c0a235be60d1d98cab71edc5932134
Sandeep Agarwal <pensande@gmail.com>
2023-09-04 17:52:39 +0000

    using webpack html plugin

--- firebase/webpack.config.js
+++ firebase/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // The entry point file described above
@@ -17,5 +18,10 @@ module.exports = {
             use: ['style-loader', 'css-loader']
         }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ]
 };
