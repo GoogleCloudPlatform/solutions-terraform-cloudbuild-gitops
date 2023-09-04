fb7cb532b1d7f9eaceeec9fee200e4a1bc5edc8a
Sandeep Agarwal <pensande@gmail.com>
2023-09-04 17:18:41 +0000

    firestore backend working

--- firebase/webpack.config.js
+++ firebase/webpack.config.js
@@ -8,7 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  // Optional and for development only. This provides the ability to
-  // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  module: {
+    rules: [{
+            test: /\.css$/,
+            include: [
+                path.resolve(__dirname, 'src')
+            ],
+            use: ['style-loader', 'css-loader']
+        }
+    ]
+  }
 };
