bff3f13bb871f460e2038d6ccf152ea5554a791c
Sandeep Agarwal <pensande@gmail.com>
2023-09-04 07:55:40 +0000

    package deployment

---
+++ firebase/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  // The entry point file described above
+  entry: './src/index.js',
+  // The location of the build folder described above
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
+};
