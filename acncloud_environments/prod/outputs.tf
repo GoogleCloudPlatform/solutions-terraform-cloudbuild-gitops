148c5278eba9417ab1e0740590b799f8ea4edb66
Nathan He <nathan.he@loreal.com>
2023-09-13 16:12:46 +0800

    Init commit

--- acncloud_environments/prod/outputs.tf
+++ acncloud_environments/prod/outputs.tf
@@ -13,9 +13,22 @@
 # limitations under the License.
 
 
-terraform {
-  backend "gcs" {
-    bucket = "acncloud-tfstate"
-    prefix = "env/dev"
-  }
+output "network" {
+  value = "${module.vpc.network}"
 }
+
+output "subnet" {
+  value = "${module.vpc.subnet}"
+}
+
+# output "firewall_rule" {
+#   value = "${module.firewall.firewall_rule}"
+# }
+
+# output "instance_name" {
+#   value = "${module.http_server.instance_name}"
+# }
+
+# output "external_ip" {
+#   value = "${module.http_server.external_ip}"
+# }
