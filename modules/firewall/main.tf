64fcbd50d99f84b27f74dcec561555e6512cdb47
Sarah ABCHICHE <43419795+itssarah@users.noreply.github.com>
2023-09-28 10:39:10 +0200

    Update main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -27,6 +27,7 @@ resource "google_compute_firewall" "allow-http" {
     ports    = ["80"]
   }
 
-  target_tags   = ["http-server"]
+  target_tags   = ["http-server2"]
   source_ranges = ["0.0.0.0/0"]
 }
+
