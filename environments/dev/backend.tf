a9dc5518ad834d1823503484064099742b8911f6
Steve Turner <steveturner@uk-lpt-ab436.lan>
2023-09-29 14:55:58 +0100

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "ab-steve-bronze-hero-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="ab-steve-bronze-hero"
