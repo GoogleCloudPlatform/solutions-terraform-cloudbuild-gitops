1fdf81deb659921c39c9d331be6d99a75281f8d9
SaberB2000 <saberbelaidi00@gmail.com>
2023-10-03 16:16:41 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "soy-storm-400821-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="soy-storm-400821"
