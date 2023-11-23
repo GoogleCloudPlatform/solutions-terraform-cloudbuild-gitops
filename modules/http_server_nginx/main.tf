a434b0581c559890f74bcbf9f8941e714d980334
heikoliedtke <heikoliedtke@google.com>
2023-11-23 15:45:16 +0100

    added nginx module

---
+++ modules/http_server_nginx/main.tf
@@ -0,0 +1,46 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+locals {
+  network = "${element(split("-", var.subnet), 0)}"
+}
+
+resource "google_compute_instance" "http_server" {
+  project      = "${var.project}"
+  zone         = "${var.region}-a"
+  name         = "${local.network}-nginx-instance"
+  machine_type = "f1-micro"
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install nginx -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot = true
+    enable_vtpm = true
+    
+  }
+  network_interface {
+    subnetwork = "${var.subnet}"
+  
+  }
+
+  # Apply the firewall rule to allow external IPs to access this instance
+  tags = ["http-server"]
+}
