03e3d54054cc8283631931698965959db8b801af
heikoliedtke <heikoliedtke@google.com>
2023-11-23 16:33:15 +0100

    added ssh rule

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -30,3 +30,17 @@ resource "google_compute_firewall" "allow-http" {
   target_tags   = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
 }
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "${local.network}-allow-ssh"
+  network = "${local.network}"
+  project = "${var.project}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["http-server"]
+  source_ranges = ["0.0.0.0/0"]
+}
