8ad8bf3eb11acdc848a8c87136b4f44ad25dd070
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2023-12-20 22:13:06 +1100

    remove vm

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -25,10 +25,3 @@ output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
 
-output "instance_name" {
-  value = "${module.http_server.instance_name}"
-}
-
-output "external_ip" {
-  value = "${module.http_server.external_ip}"
-}
