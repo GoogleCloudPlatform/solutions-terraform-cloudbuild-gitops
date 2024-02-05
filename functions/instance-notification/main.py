02ebb29a011eecb03cf476771bce484409ab1422
Sandeep Agarwal <pensande@gmail.com>
2024-02-05 06:27:58 +0000

    fine-tuning instance resource tag function

--- functions/instance-notification/main.py
+++ functions/instance-notification/main.py
@@ -3,6 +3,7 @@ import json
 import base64
 import requests
 from google.cloud import asset_v1
+from google.cloud import resourcemanager_v3
 
 def instance_notification(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -14,100 +15,28 @@ def instance_notification(event, context):
     message_json = json.loads(pubsub_message)
     print(message_json)
 
-    client      = asset_v1.AssetServiceClient()
-    response    = client.search_all_resources(
-        request = {
-            "scope": "projects/pensande",
-            "query": "NOT tagKeys:network",
-            "asset_types": ["compute.googleapis.com/Instance"],
-            "format": "table(name, assetType)"
-        }
-    )
-    for resource in response:
-        print(resource)
-
-    '''
-    delta = []
-        
-    for new_binding in message_json["asset"]["iamPolicy"]["bindings"]:
-        if new_binding not in message_json["priorAsset"]["iamPolicy"]["bindings"]:
-            old_binding = next((b for b in message_json["priorAsset"]["iamPolicy"]["bindings"] if b["role"] == new_binding["role"]), "no_old_binding")
-            print(f"Found New Binding: {new_binding} with Old Binding: {old_binding}")
-            if old_binding == "no_old_binding": 
-                # no old binding hence report new binding as is
-                delta.append(new_binding)
-            else:
-                # compare against old binding and find new members
-                members = []
-                for new_member in new_binding["members"]:
-                    if new_member not in old_binding["members"]:
-                        members.append(new_member)
-                if members:
-                    delta.append({
-                        "members": members,
-                        "role": new_binding["role"]
-                    })
-                
-    if delta:
-        print(f"Delta: {delta}")
-        assetType = message_json["asset"]["assetType"].split("/")[-1]
-        assetName = message_json["asset"]["name"].split("/")[-1]
-        send_slack_chat_notification(assetType, assetName, delta)
-    '''
-        
-def send_slack_chat_notification(assetType, assetName, delta):
     try:
-        slack_message = [
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": f"IAM Policy Grant Alert!"
-                    }
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*{assetType}:* {assetName}"
-                        }
-                    ]
-                }
-            ]
+        search_asset_client     = asset_v1.AssetServiceClient()
+        search_asset_request    = asset_v1.SearchAllResourcesRequest(
+            scope="projects/pensande",
+            query="NOT tagKeys:network",
+            asset_types="compute.googleapis.com/Instance",
+            format="value(name)"
+        )
+        search_asset_result = search_asset_client.search_all_resources(request = search_asset_request)
+        for search_asset in search_asset_result:
+            print(search_asset)
+    except Exception as error:
+        print(f"Error in listing non-compliant instances: {error}")
 
-        for binding in delta:
-            slack_message.append(
-                {
-                    "type": "divider"
-                }
-            )
-            slack_message.append(
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Members:*\n{binding['members']}"
-                        },
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Role:*\n{binding['role']}"
-                        }
-                    ]
-                }
-            )
+    try:
+        tag_bindings_client     = resourcemanager_v3.TagBindingsClient()
+        tag_bindings_request    = resourcemanager_v3.ListTagBindingsRequest(
+            parent = message_json["asset"]["name"]
+        )
+        tag_bindings_result     = tag_bindings_client.list_tag_bindings(request = tag_bindings_request)
+        for tag_bindings in tag_bindings_result:
+            print(tag_bindings)
+    except Exception as error:
+        print(f"Error in listing tag bindings: {error}")
     
-        slack_token = os.environ.get('SLACK_ACCESS_TOKEN', 'Specified environment variable is not set.')
-        slack_channel = os.environ.get('SLACK_CHANNEL', 'Specified environment variable is not set.')
-        response = requests.post("https://slack.com/api/chat.postMessage", data={
-            "token": slack_token,
-            "channel": slack_channel,
-            "text": "IAM Policy Grant Alert!",
-            "blocks": json.dumps(slack_message)
-        })
-        print(f"Slack responded with Status Code: {response.status_code}")
-        return True
-    except Exception as e:
-        print(e)
-        raise(e)
