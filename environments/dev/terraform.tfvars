e2eeda1b7034a63f238a42a0117c80efd52e40ab
piotrlg <piotrlg@gmail.com>
2024-03-12 10:07:28 +0100

    update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="gitops-417008"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "gitops-417008-tfstate"
     prefix = "env/prod"
   }
 }
